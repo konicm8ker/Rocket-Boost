545d368da95c1df68d1c4591b8db74cc8319fd86
Tommy Morris <konicm8ker@gmail.com>
2020-07-23 00:09:11 -0500

    Added script to handle input and added thrust movement to control rocket. (9_pb_cu2)

---
+++ Assets/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space)) {
+			print("Thrust engaged!"); // DEBUG
+			rb.AddRelativeForce(Vector3.up);
+		}
+
+		if(Input.GetKey(KeyCode.A)) {
+			print("Rotating left.");  // DEBUG
+		} else if(Input.GetKey(KeyCode.D)) {
+			print("Rotating right.");  // DEBUG
+		}
+    }
+}
