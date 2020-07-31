c5e7234e5a63940dec10e5134f629724cb87067d
Tommy Morris <konicm8ker@gmail.com>
2020-07-31 01:06:54 -0500

    Added camera controller logic to follow rocket when needed.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform target;
+	public float offset = 26f;
+	public float speed = 0.125f;
+
+	void LateUpdate()
+	{
+		if(target.position.y > (transform.position.y + offset) && !(transform.position.y > 72)) // If rocket hits top trigger
+		{
+			transform.position = new Vector3(transform.position.x,(transform.position.y + speed),transform.position.z);
+		}
+		else if((target.position.y < (transform.position.y - offset)) && !(transform.position.y < 22)) // If rocket hits bottom trigger
+		{
+			transform.position = new Vector3(transform.position.x,(transform.position.y - speed),transform.position.z);
+		}
+	}
+
+}
