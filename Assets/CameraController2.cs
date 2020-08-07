eae596d46de2f6643f110d5152909f7b8f5d6c58
Tommy Morris <konicm8ker@gmail.com>
2020-08-06 22:34:18 -0500

    Created and finished level 7 design and added custom cam controller for level 7.

---
+++ Assets/CameraController2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraController2 : MonoBehaviour {
+
+	[SerializeField] Transform target;
+	[SerializeField] float offset = 14f;
+	[SerializeField] float speed = 0.25f;
+
+	void LateUpdate()
+	{
+		if(target.position.y > (transform.position.y + offset) && !(transform.position.y > 289)) // If rocket hits top trigger
+		{
+			transform.position = new Vector3(transform.position.x,(transform.position.y + speed),transform.position.z);
+		}
+		else if((target.position.y < (transform.position.y - offset)) && !(transform.position.y < 22)) // If rocket hits bottom trigger
+		{
+			transform.position = new Vector3(transform.position.x,(transform.position.y - speed),transform.position.z);
+		}
+
+	}
+
+}
